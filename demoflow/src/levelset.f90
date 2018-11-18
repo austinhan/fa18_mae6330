7f3eee84ebc15d5a686efb468e4524371c090d2d
austinhan <han.austin@outlook.com>
2018-11-18 15:12:33 -0500

    Added deformed disk

--- demoflow/src/levelset.f90
+++ demoflow/src/levelset.f90
@@ -37,9 +37,10 @@ subroutine levelset_init
   ! Loop over full field and create initial distance - Zalesak
   do j=1,ny
      do i=1,nx
-        G(i,j)=-sqrt(xm(i)**2+ym(j)**2)+0.15
+        !G(i,j)=-sqrt(xm(i)**2+ym(j)**2)+0.15
         !if ((xm(i)**2+ym(j)**2).lt.0.15) G(i,j)=-G(i,j)
         !G(i,j)=init_zalesak((/xm(i),ym(j),0.0_WP/),(/0.0_WP,0.25_WP,0.0_WP/),0.15_WP,0.05_WP,0.25_WP)
+        G(i,j)=init_deformed((/xm(i),ym(j),0.0_WP/),0.25_WP,0.05_WP)
      end do
   end do
   
@@ -86,6 +87,16 @@ contains
        init_zalesak = c  
     endif
   end function init_zalesak
+
+  function init_deformed(xyz,r0,alpha)
+    implicit none
+    real(WP) :: init_deformed
+    real(WP), dimension(3), intent(in) :: xyz
+    real(WP), intent(in) :: r0,alpha
+    real(WP) :: r
+    r = r0*(1.0_WP+alpha*cos(2.0_WP*atan2(xyz(2),xyz(1))))
+    init_deformed = r-(xyz(1)**2+xyz(2)**2)**0.5_WP
+  end function init_deformed
   
 end subroutine levelset_init
 
